b63abc13b6a3f7852a8ad505d3dd6197d892318e
samuelvarejames <samuelvarejames@gmail.com>
2022-12-15 12:49:57 +0100

    Remove sqlite and use poetry run

--- pad/jotpad/settings/dev.py
+++ pad/jotpad/settings/dev.py
@@ -2,10 +2,9 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-SQLITE = False
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ['*']
 
 SITE_ID = 1
 
@@ -13,30 +12,22 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
-if SQLITE is True:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRESDB_HOST'),
+        'PORT': os.environ.get('POSTGRESDB_PORT'),
+        'TEST': {
+            'CHARSET': None,
+            'COLLATION': None,
+            'NAME': os.path.join(os.path.dirname(__file__), 'test.db'),
+            'MIRROR': None
         }
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ.get('POSTGRES_DB'),
-            'USER': os.environ.get('POSTGRES_USER'),
-            'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-            'HOST': os.environ.get('POSTGRESDB_HOST'),
-            'PORT': os.environ.get('POSTGRESDB_PORT'),
-            'TEST': {
-                'CHARSET': None,
-                'COLLATION': None,
-                'NAME': os.path.join(os.path.dirname(__file__), 'test.db'),
-                'MIRROR': None
-            }
-        }
-    }
+}
 
 
 # Static files (CSS, JavaScript, Images)
